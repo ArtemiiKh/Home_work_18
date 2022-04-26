e0de9b0773534764b4cb0f1bf6eac7ed1eb14d7e
ArtemiiKh <68230202+ArtemiiKh@users.noreply.github.com>
2022-04-26 19:31:43 +0300

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -3,6 +3,9 @@ function mainFunction (callback) {
 	const deg = +prompt("Введите степень числа");
 
 	return exponentiation(num, deg);
+	// return multiplay(num, deg);
+	// return division(num, deg);
+	// return modulo(num, deg);
 }
 
 function exponentiation (num, deg) {
@@ -12,4 +15,28 @@ function exponentiation (num, deg) {
 	return degResult;
 }
 
+// function multiplay (num, deg) {
+
+// 	const numInDeg = num * deg;
+// 	alert(numInDeg);
+// 	return numInDeg;
+// }
+
+// function division (num, deg) {
+
+// 	const numInDeg = (num / deg).toFixed(2);
+// 	alert(numInDeg);
+// 	return numInDeg;
+// }
+
+// function modulo (num, deg) {
+
+// 	const numInDeg = num % deg;
+// 	alert(numInDeg);
+// 	return numInDeg;
+// }
+
 console.log(mainFunction(exponentiation));
+// console.log(mainFunction(multiplay));
+// console.log(mainFunction(division));
+// console.log(mainFunction(modulo));
